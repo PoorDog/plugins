ecec585563be0218b8e3d4acf60d17f5fc182158
Poorchop <poorchop@riseup.net>
2014-06-18 16:40:48 -0400

    Update statuscolor.py

--- HexChat/statuscolor.py
+++ HexChat/statuscolor.py
@@ -7,35 +7,39 @@ __module_description__ = 'Colors nicks based on user mode'
 # This is just a direct port of LifeIsPain's statuscolor.pl script.
 
 events = ('Channel Message',
-		'Channel Action',
-		'Channel Msg Hilight',
-		'Channel Action Hilight',
-		'Your Message',
-		'Your Action')
+          'Channel Action',
+          'Channel Msg Hilight',
+          'Channel Action Hilight',
+          'Your Message',
+          'Your Action')
 
 # TODO: update these for hexchats default theme
-modes = {'+':'24',
-		'%':'28',
-		'@':'23',
-		'&':'21',
-		'~':'22'}
+modes = {'+': '30',
+         '%': '30',
+         '@': '30',
+         '&': '30',
+         '~': '30'}
 
 edited = False
 
+
 def msg_cb(word, word_eol, event, attrs):
-	global edited
-	if edited or len(word) < 3 or not word[2] in modes:
-		return hexchat.EAT_NONE
+    global edited
+    if edited or len(word) < 3 or not word[2] in modes:
+        return hexchat.EAT_NONE
 
-	color = modes[word[2]]
-	nick = '\003{}{}\017'.format(color, hexchat.strip(word[0]))
-	word = [(word[i] if len(word) > i else '') for i in range(4)]
+    color = modes[word[2]]
+    nick = '\003{}{}\017'.format(color, hexchat.strip(word[0]))
+    word = [(word[i] if len(word) > i else '') for i in range(4)]
+    if event == 'Channel Msg Hilight' or event == 'Channel Action Hilight':
+        word[1] = '\00325{}\017'.format(word[1])
 
-	edited = True
-	hexchat.emit_print(event, nick, word[1], word[2], word[3], time=attrs.time)
-	edited = False
+    edited = True
+    hexchat.emit_print(event, nick, word[1], word[2], word[3], time=attrs.time)
+    edited = False
 
-	return hexchat.EAT_ALL
+    return hexchat.EAT_ALL
+
 
 for event in events:
-	hexchat.hook_print_attrs(event, msg_cb, event, hexchat.PRI_HIGH)
+    hexchat.hook_print_attrs(event, msg_cb, event, hexchat.PRI_HIGH)
